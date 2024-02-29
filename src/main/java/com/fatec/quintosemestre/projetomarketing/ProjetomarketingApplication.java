2d42c294a0faf52445efb07db648b4e6b755f790
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-02-29 10:44:32 -0300

    first commit

---
+++ src/main/java/com/fatec/quintosemestre/projetomarketing/ProjetomarketingApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.quintosemestre.projetomarketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomarketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/quintosemestre/projetomarketing/ProjetomarketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.quintosemestre.projetomarketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomarketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
