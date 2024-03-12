aef0a84f5b06affcb4d789a7eaa3199ad665d407
Adriano Donini <2003donini@gmail.com>
2024-03-12 20:21:01 -0300

    style: removed unused import

--- src/main/java/com/fatec/quintosemestre/projetomarketing/repository/MensagemRepository.java
+++ src/main/java/com/fatec/quintosemestre/projetomarketing/repository/MensagemRepository.java
@@ -1,7 +1,6 @@
 package com.fatec.quintosemestre.projetomarketing.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.fatec.quintosemestre.projetomarketing.model.Mensagem;
 
